e5702b25cb710606d7d264832e363277d9cf46f7
HyNS00 <hsk7953@gmail.com>
2025-12-12 17:33:10 +0900

    Initial commit

---
+++ src/main/java/com/hyuns/cafit/CafitApplication.java
@@ -0,0 +1,13 @@
+package com.hyuns.cafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyuns/cafit/CafitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyuns.cafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
