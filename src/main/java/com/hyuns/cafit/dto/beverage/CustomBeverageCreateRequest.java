0b90f5c22086c74aeb7eb94c26d15180ba7eded8
HyNS00 <hsk7953@gmail.com>
2025-12-16 16:48:35 +0900

    feat(CustomBeverageCreateRequest): 음료 용량에 필드에 MAX 유효성 검증 추가

--- src/main/java/com/hyuns/cafit/dto/beverage/CustomBeverageCreateRequest.java
+++ src/main/java/com/hyuns/cafit/dto/beverage/CustomBeverageCreateRequest.java
@@ -1,14 +1,11 @@
 package com.hyuns.cafit.dto.beverage;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 
 public record CustomBeverageCreateRequest(
-        @NotNull(message = "사용자 ID는 필수입니다.")
-        Long userId,
 
         @NotBlank(message = "음료 이름은 필수입니다")
+        @Size(max = 50, message = "음료 이름은 50자 이하여야 합니다")
         String name,
 
         @NotNull(message = "카테고리는 필수입니다")
@@ -16,10 +13,10 @@ public record CustomBeverageCreateRequest(
 
         @NotNull(message = "용량은 필수입니다")
         @Positive(message = "용량은 0보다 커야 합니다")
+        @Max(value = 10000, message = "용량은 10000ml 이하여야 합니다")
         Integer volumeMl,
 
         @NotNull(message = "카페인량은 필수입니다")
         @Positive(message = "카페인량은 0보다 커야 합니다")
         Double caffeineMg
-) {
-}
+) {}
