72e53bc95e1848c992fc67bfaa38596aaeff0683
HyNS00 <hsk7953@gmail.com>
2025-12-17 20:37:32 +0900

    refactor: 카페인 타임라인을 사용하도록 수정

--- src/main/java/com/hyuns/cafit/dto/statistics/CaffeineTimelineResponse.java
+++ src/main/java/com/hyuns/cafit/dto/statistics/CaffeineTimelineResponse.java
@@ -4,10 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record CaffeineTimelineResponse(
-        List<DataPoint> dataPoints,
+        List<TimelineDataPoint> dataPoints,
         LocalDateTime currentTime,
         LocalDateTime bedtime,
         double targetSleepCaffeine
-) {
-    public record DataPoint(LocalDateTime time, double caffeineMg){}
-}
+) {}
