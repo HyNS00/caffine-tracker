d581c92a83b6cc8fad62b90c3280a26a55e532bd
HyNS00 <hsk7953@gmail.com>
2025-12-19 15:51:45 +0900

    feat: 즐겨찾기 음료 생성 dto 구현

---
+++ src/main/java/com/hyuns/cafit/dto/favorite/FavoriteCreateRequest.java
@@ -0,0 +1,12 @@
+package com.hyuns.cafit.dto.favorite;
+
+import com.hyuns.cafit.domain.beverage.BeverageType;
+import jakarta.validation.constraints.NotNull;
+
+public record FavoriteCreateRequest(
+        @NotNull(message = "음료 카테고리는 필수입니다")
+        BeverageType category,
+
+        @NotNull(message = "음료 ID는 필수입니다")
+        Long beverageId
+) {}
