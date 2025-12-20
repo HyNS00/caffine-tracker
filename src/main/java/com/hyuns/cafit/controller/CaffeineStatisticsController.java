8e1576ebcc3b44266279cd9bc3468251cbd03908
HyNS00 <hsk7953@gmail.com>
2025-12-20 11:26:18 +0900

    feat: top3 음료 찾기 로직 컨트롤러 추가

--- src/main/java/com/hyuns/cafit/controller/CaffeineStatisticsController.java
+++ src/main/java/com/hyuns/cafit/controller/CaffeineStatisticsController.java
@@ -4,6 +4,7 @@ import com.hyuns.cafit.auth.Login;
 import com.hyuns.cafit.domain.user.User;
 import com.hyuns.cafit.dto.statistics.CaffeineTimelineResponse;
 import com.hyuns.cafit.dto.statistics.DailyStatisticsResponse;
+import com.hyuns.cafit.dto.statistics.TopBeverageStat;
 import com.hyuns.cafit.service.CaffeineStatisticsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,5 +37,13 @@ public class CaffeineStatisticsController {
     ) {
         DailyStatisticsResponse response = statisticsService.getDailyStatistics(user, days);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/top-beverages")
+    public ResponseEntity<List<TopBeverageStat>> getTopBeverages(
+            @Login User user,
+            @RequestParam(defaultValue = "7") int days
+    ) {
+        return ResponseEntity.ok(statisticsService.getTopBeverages(user, days));
     }
 }
